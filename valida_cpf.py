e1bfd656c7b68f9cb08a391ac2da45937611be4f
Kevinrehem <kevin.rehem@gmail.com>
2025-09-27 16:06:16 -0300

    validador_cpf

--- valida_cpf.py
+++ valida_cpf.py
@@ -1,4 +1,22 @@
-
+def calcula_verificador(cpf, type):
+    dig=0
+    last = 0
+    peso=10
+    if(type == 1):
+        last = len(cpf) - 2
+    elif(type == 2):
+        last = len(cpf) - 1
+        peso=11
+        
+    for i in range(last):
+        dig+=int(cpf[i])*peso
+        peso-=1
+    dig%=11
+    if dig <= 1:
+        dig = 0
+    else:
+        dig = 11 - dig
+    return dig
 
 def valida_cpf(cpf):
     if len(cpf) != 14:
@@ -8,6 +26,12 @@ def valida_cpf(cpf):
     cpf = cpf.replace('.', '')
     cpf = cpf.replace('-', '')
     if not cpf.isdigit():
+        return False
+    if calcula_verificador(cpf, 1) != int(cpf[-2]):
+        return False
+    if calcula_verificador(cpf, 2) != int(cpf[-1]):
+        return False
+    if cpf == 11*cpf[0]:
         return False
     return True        
 
